53d6d845989291da98f657e5b162810c19dbae95
WillGiab <2410250284@qq.com>
2018-02-05 00:03:10 +0800

    基于Entitas的ECS框架搭建

---
+++ WTClient/Assets/Sources/Logic/WTEngineSystems.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WTEngineSystems : Feature {
+	public WTEngineSystems(Contexts contexts){
+		//在这里注册所有的Systems
+
+		//input
+
+		//Update
+
+		//Render
+
+		//Destroy
+
+
+	}
+
+}
