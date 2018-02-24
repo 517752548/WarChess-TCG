9265c5a23e146cec4c27796a3c6b55e4ed998a69
WillGiab <2410250284@qq.com>
2018-02-24 22:00:33 +0800

    init

---
+++ WTClient/Assets/Scripts/Tbs Framework/Cell/Interface/ICellGridGenerator.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ICellGridGenerator : MonoBehaviour
+{
+	public Transform CellsParent;
+	public abstract List<Cell> GenerateGrid();
+}
---
+++ WTClient/Assets/Scripts/Tbs Framework/Cell/Interface/IGraphNode.cs
@@ -0,0 +1,7 @@
+namespace Tbs_Framework
+{
+	public interface IGraphNode
+	{
+		int GetDistance(IGraphNode graphNode);
+	}
+}
